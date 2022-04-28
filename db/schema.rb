e0ff1e223bf45d64bbd21b216ced56051ce2f100
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-04-28 19:43:01 -0300

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_185101) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_19_185101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +20,8 @@ ActiveRecord::Schema.define(version: 2022_01_19_185101) do
     t.string "cpf"
     t.string "email"
     t.string "telefone"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
